8862692f249d47da3f5029a7226a0cfe479fa2ee
bibicobolado <rodrigosiqueira@id.uff.br>
2019-04-02 16:21:53 -0300

    separando aula05

--- cpf_cnpj.py
+++ cpf_cnpj.py
@@ -1,8 +1,9 @@
 from validate_docbr import CPF,CNPJ
 
-class DocFactory:
+class Documento:
+
     @staticmethod
-    def factory(doc):
+    def criarNovo(doc):
         docStr = str(doc)
         if len(docStr) == 14:
             return DocCnpj(docStr)
@@ -13,42 +14,44 @@ class DocFactory:
 
 class DocCpf:
     def __init__(self,cpf):
-        if self.validaCpf(cpf):
+        if self.valida(cpf):
             self.cpf = cpf
         else:
             raise ValueError("CPF inválido")
 
     def __str__(self):
-        return self.formatCpf(self.cpf)
+        return self.formata(self.cpf)
 
-    def validaCpf(self,cpf):
+    def valida(self,cpf):
         valida_cpf = CPF()
         return valida_cpf.validate(cpf)
 
     @staticmethod
-    def formatCpf(cpf):
+    def formata(cpf):
         mascara_cpf = CPF()
         return mascara_cpf.mask(cpf)
 
 
 class DocCnpj:
     def __init__(self, cnpj):
-        if self.validaCnpj(cnpj):
+        if self.valida(cnpj):
             self.cnpj = cnpj
         else:
             raise ValueError("CNPJ Inválido")
 
     def __str__(self):
-        return self.formatCnpj(self.cnpj)
+        return self.formata(self.cnpj)
 
-    def validaCnpj(self, cnpj):
+    def valida(self, cnpj):
         valida_cnpj = CNPJ()
         return valida_cnpj.validate(cnpj)
 
     @staticmethod
-    def formatCnpj(cnpj):
+    def formata(cnpj):
         mascara_cnpj = CNPJ()
         return mascara_cnpj.mask(cnpj)
 
-cpfRodrigo = DocFactory.factory(20881175000162)
-print(cpfRodrigo)
+cnpj = Documento.criarNovo(20881175000162)
+cpf  = Documento.criarNovo(15316264754)
+print(cpf)
+print(cnpj)
